9f23094bb8b13d77d1c6bb8f56e8a8d238017fce
Agtna <putrilevinaagt@gmail.com>
2026-03-15 22:31:48 +0700

    Create simplecalculator.dart

---
+++ dartintro/simplecalculator.dart
@@ -0,0 +1,17 @@
+void main(){
+  int a = 20;
+  int b = 4;
+
+  int penjumlahan = a + b;
+  int pengurangan = a - b;
+  int perkalian = a * b;
+  double pembagian = a / b;
+  int pembagianBulat = a ~/ b;
+
+  print("simple calculator");
+  print("Hasil penjumlahan = $penjumlahan");
+  print("Hasil pengurangan = $pengurangan");
+  print("Hasil perkalian = $perkalian");
+  print("Hasil pembagian = $pembagian");
+  print("Hasil pembagianBulat = $pembagianBulat");
+}
