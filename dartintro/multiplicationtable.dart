413fb927dd81a29663ffa36d4dde2ccd473a39d7
Agtna <putrilevinaagt@gmail.com>
2026-03-15 22:40:37 +0700

    Create multiplicationtable.dart

---
+++ dartintro/multiplicationtable.dart
@@ -0,0 +1,10 @@
+void main() {
+  int number = 5; 
+
+  print("=== Tabel Perkalian $number ===");
+
+  for (int i = 1; i <= 10; i++) {
+    int hasil = number * i;
+    print("$number x $i = $hasil");
+  }
+}
