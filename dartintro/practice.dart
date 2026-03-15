60d1c9992aefa5533b3a2bd1c3d946a9cb2aa114
Agtna <putrilevinaagt@gmail.com>
2026-03-15 22:14:50 +0700

    Create practice.dart

---
+++ dartintro/practice.dart
@@ -0,0 +1,9 @@
+void main (){
+  String name = "Putri Levina Agatha";
+  String studentId = "4.33.24.1.20";
+  String major = "Teknologi Rekayasa Komputer";
+
+  print("Nama saya $name");
+  print("NIM saya $studentId");
+  print("Prodi saya $major");
+}
